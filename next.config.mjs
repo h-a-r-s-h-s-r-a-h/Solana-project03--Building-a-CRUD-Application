a1953621fa428d92f0cf3d1c2607175f00a7a0af
Harsh <harshvirat894@gmail.com>
2025-01-30 16:01:49 +0530

    Project03

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false, // Prevents errors related to `fs` in the browser
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
